20bc40841f231d45b5a9c827d5d695c945c095ea
yosuke-oka <y.oka711@gmail.com>
2022-03-25 14:11:28 +0900

    not work

--- src/test/java/org/embulk/output/kintone/TestKintoneOutputPlugin.java
+++ src/test/java/org/embulk/output/kintone/TestKintoneOutputPlugin.java
@@ -1,5 +1,37 @@
 package org.embulk.output.kintone;
 
+import static org.junit.Assert.assertEquals;
+
+import org.embulk.config.ConfigSource;
+import org.embulk.spi.FileInputPlugin;
+import org.embulk.spi.OutputPlugin;
+import org.embulk.spi.ParserPlugin;
+import org.embulk.standards.CsvParserPlugin;
+import org.embulk.standards.LocalFileInputPlugin;
+import org.embulk.test.TestingEmbulk;
+import org.junit.Rule;
+import org.junit.Test;
+
+
+
 public class TestKintoneOutputPlugin
 {
+
+    @Rule
+    public TestingEmbulk embulk = TestingEmbulk.builder()
+            .registerPlugin(FileInputPlugin.class, "file", LocalFileInputPlugin.class)
+            .registerPlugin(ParserPlugin.class, "csv", CsvParserPlugin.class)
+            .registerPlugin(OutputPlugin.class, "kintone", KintoneOutputPlugin.class)
+            .build();
+
+
+    @Test
+    public void testConfigDefault() {
+        final ConfigSource config = embulk.configLoader()
+                                          .newConfigSource()
+                                          .set("type", "kintone");
+        final PluginTask task = config.loadConfig(PluginTask.class);
+
+        assertEquals("insert", task.getMode());
+    }
 }
