647f1ffe695dc8cb0db8bef90e13f3908a53a2f4
Yas Okada <yas-okadatech@users.noreply.github.com>
2026-04-22 14:38:21 +0900

    Revert "Add structured error logging for failed Kintone API records"

--- src/main/java/org/embulk/output/kintone/PluginTask.java
+++ src/main/java/org/embulk/output/kintone/PluginTask.java
@@ -89,10 +89,6 @@ public interface PluginTask extends Task {
   @ConfigDefault("{}")
   KintoneRetryOption getRetryOptions();
 
-  @Config("error_records_detail_output_file")
-  @ConfigDefault("null")
-  Optional<String> getErrorRecordsDetailOutputFile();
-
   Set<Column> getDerivedColumns();
 
   void setDerivedColumns(Set<Column> columns);
